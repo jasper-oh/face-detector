fcabd1b338741156f63006999f193057339e4084
jasper <63331153+jasper-oh@users.noreply.github.com>
2021-01-02 14:19:18 +0900

    Video Capture

--- face_detector.py
+++ face_detector.py
@@ -2,18 +2,25 @@ import cv2
 from random import randrange
 
 trained_face_data = cv2.CascadeClassifier(
-    'haarcascade_frontalface_default.xml')
+    'haarcascade_frontalface_default.xml')q
 
-img = cv2.imread('RDJ.png')
+# img = cv2.imread('RDJ.png')
+# TO capture video from webcam
+webcam = cv2.VideoCapture(0)
 
-grayscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+while True:
 
-face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
+    successful_frame_read, frame = webcam.read()
+    grayscaled_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
 
-for (x, y, w, h) in face_coordinates:
-    cv2.rectangle(img, (x, y), (x+w, y+h), (randrange(256),
-                                            randrange(256), randrange(256)), 10)
+    for (x, y, w, h) in face_coordinates:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 10)
 
+    cv2.imshow('Jasper Face Detector', frame)
+    key = cv2.waitKey(1)
 
-cv2.imshow('Jasper Face Detector', img)
-cv2.waitKey()
+    if key == 81 or key == 113:
+        break
+
+webcam.release()
